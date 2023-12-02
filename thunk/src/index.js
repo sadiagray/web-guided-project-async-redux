f43ce87c9355ff49cb58c06f2533e9fc8eb54bc8
Sadia Gray <sadiagray17@gmail.com>
2023-12-02 14:33:41 -0600

    finished follow along

--- thunk/src/index.js
+++ thunk/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 
 import { reducer } from './reducers';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+import { applyMiddleware } from 'redux';
+import { compose } from 'redux';
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
